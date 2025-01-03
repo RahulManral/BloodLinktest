169e66ac73b4be2aa1cbac78f7acebac93e41aa4
NinjaSucksAtLife <6rahulmanral9@gmail.com>
2025-01-03 21:05:37 +0800

    commit

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 });
